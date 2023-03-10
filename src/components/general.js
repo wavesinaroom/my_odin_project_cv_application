173798645e6950cf70d47574867023eeba2fc709
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-10 12:43:56 -0500

    [Task] Test module import with general.js

---
+++ src/components/general.js
@@ -0,0 +1,13 @@
+function General(){
+
+
+  return(
+    <div>
+      <label for="name">Candidate&apos;s name</label>
+      <input type="text" id="name" name="name" required size="10"/>
+    </div>
+  );
+}
+
+export default General;
+
