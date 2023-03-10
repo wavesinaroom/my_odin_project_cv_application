24cda6ecc6ee9580508f605547b47a7d5946b9f5
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-10 14:44:13 -0500

    [Feature] General info component - HTML

--- src/components/general.js
+++ src/components/general.js
@@ -5,6 +5,10 @@ function General(){
     <div>
       <label for="name">Candidate&apos;s name</label>
       <input type="text" id="name" name="name" required size="10"/>
+      <label for="phone">Phone</label>
+      <input type="text" id="phone" name="phone" required size="10"/>
+      <label for="e-mail">E-mail</label>
+      <input type="e-mail" id="e-mail" name="e-mail" required size="10"/>
     </div>
   );
 }
