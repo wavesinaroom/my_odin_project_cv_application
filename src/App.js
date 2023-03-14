efe4a826dd8a0f010366b99eac2b15fac0584332
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-14 12:10:54 -0500

    [Feature] Render HTML content on submit click

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import {useState} from 'react';
+import {renderToStaticMarkup} from 'react-dom/server'
 import General from "./components/general";
 import Section from "./components/section";
 
 function App() {
+  const [render,setRender] = useState('');
+
   return (
     <>
       <General/>
       <Section title="Education"/>
       <Section title="Work"/>
+      <input type="submit" value="submit" onClick={()=>setRender(renderToStaticMarkup(<App/>))}/>
+      {render}
     </>
   );
 }
