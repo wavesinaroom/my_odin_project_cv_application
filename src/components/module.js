caeb70d819fc5b53e12f3553269174d14d1cf7a9
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-17 12:45:59 -0500

    [Task] Add same implementation to the rest of the modules -> it's tested!

--- src/components/module.js
+++ src/components/module.js
@@ -5,6 +5,7 @@ function Module(props){
   const [one,setOne] = useState('');
   const [two,setTwo] = useState('');
   const [three,setThree] = useState('');
+  const [four,setFour] = useState('');
 
   switch(title){
     case "Certification":
@@ -22,13 +23,13 @@ function Module(props){
       return(
         <li>
           <label for="company">Company</label>
-          <input type="text" className="company" name="company" size="10" />      
+          <input type="text" className="company" name="company" size="10" value={one} onChange={(e)=>setOne(e.target.value)} />      
           <label for="position">Position</label>
-          <input type="text" className="position" name="position" size="10"/>      
+          <input type="text" className="position" name="position" size="10" value={two} onChange={(e)=>setTwo(e.target.value)}/>      
           <label for="from">From</label>
-          <input type="date" className="from" name="from" size="10"/>      
+          <input type="date" className="from" name="from" size="10" value={three} onChange={(e)=>setThree(e.target.value)}/>      
           <label for="until">Until</label>
-          <input type="date" className="until" name="until" size="10"/>      
+          <input type="date" className="until" name="until" size="10" value={four} onChange={(e)=>setFour(e.target.value)}/>      
           <h3>Tasks</h3>
         </li>
       );
@@ -36,17 +37,17 @@ function Module(props){
       return(
         <li>
           <label for="name">Candidate&apos;s name</label>
-          <input type="text" className="name" name="name" required size="10"/>
+          <input type="text" className="name" name="name" required size="10" value={one} onChange={(e)=>setOne(e.target.value)}/>
           <label for="phone">Phone</label>
-          <input type="tel" className="phone" name="phone" required size="10"/>
+          <input type="tel" className="phone" name="phone" required size="10" value={two} onChange={(e)=>setTwo(e.target.value)}/>
           <label for="e-mail">E-mail</label>
-          <input type="email" className="e-mail" name="e-mail" required size="10"/>
+          <input type="email" className="e-mail" name="e-mail" required size="10" value={three} onChange={(e)=>setThree(e.target.value)}/>
         </li>
       );
     case "Task":
       return(
         <li>
-          <input type="text" className="task" name="task" />
+          <input type="text" className="task" name="task" value={one} onChange={(e)=>setOne(e.target.value)}/>
         </li>
       ) ;  
     default:
