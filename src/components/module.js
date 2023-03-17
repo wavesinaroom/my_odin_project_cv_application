b0d96da76a3c9d0290954a5793d1f20a3160fc44
wavesinaroom <jaureguij@javeriana.edu.co>
2023-03-17 10:48:08 -0500

    [Feature] Store inputs and reflect changes on markup output

--- src/components/module.js
+++ src/components/module.js
@@ -1,48 +1,53 @@
+import {useState} from "react";
 
 function Module(props){
   const title = props.title;
+  const [one,setOne] = useState('');
+  const [two,setTwo] = useState('');
+  const [three,setThree] = useState('');
+  const [four,setFour] = useState('');
 
   switch(title){
     case "Certification":
       return(
         <li >
-          <label for="school">School</label>
-          <input id="module" type="text" className="certification" name="school" size="10"  />
-          <label for="title">Title</label>
-          <input type="text" className="certification" name="title" size="10" />
-          <label for="Date">Date</label>
-          <input type="date" className="certification" name="date" size="10" />
+          <label for="one">School</label>
+          <input type="text" className="certification" name="one" size="10" value={one} onChange={e => setOne(e.target.value)}/>
+          <label for="two">Title</label>
+          <input type="text" className="certification" name="two" size="10" value={two} onChange={e=> setTwo(e.target.value)}/>
+          <label for="three">Date</label>
+          <input type="date" className="certification" name="three" size="10" value={three} onChange={e=> setThree(e.target.value)}/>
         </li>
       );
     case "Experience":
       return(
         <li>
-          <label for="company">Company</label>
-          <input type="text" className="company" name="company" size="10" />      
-          <label for="position">Position</label>
-          <input type="text" className="position" name="position" size="10"/>      
-          <label for="from">From</label>
-          <input type="date" className="from" name="from" size="10"/>      
-          <label for="until">Until</label>
-          <input type="date" className="until" name="until" size="10"/>      
+          <label for="one">Company</label>
+          <input type="text" className="company" name="one" size="10" value={one} onChange={e=>setOne(e.target.value)} />      
+          <label for="two">Position</label>
+          <input type="text" className="position" name="two" size="10" value={two} onChange={e=>setTwo(e.target.value)}/>      
+          <label for="three">From</label>
+          <input type="date" className="from" name="three" size="10" value={three} onchange={e=>setThree(e.target.value)}/>      
+          <label for="four">Until</label>
+          <input type="date" className="until" name="four" size="10" value={four} onChange={e=>setFour(e.target.value)}/>      
           <h3>Tasks</h3>
         </li>
       );
     case "General":
       return(
         <li>
-          <label for="name">Candidate&apos;s name</label>
-          <input type="text" className="name" name="name" required size="10"/>
-          <label for="phone">Phone</label>
-          <input type="tel" className="phone" name="phone" required size="10"/>
-          <label for="e-mail">E-mail</label>
-          <input type="email" className="e-mail" name="e-mail" required size="10"/>
+          <label for="one">Candidate&apos;s name</label>
+          <input type="text" className="name" name="one" size="10" value={one} onChange={e=>setOne(e.target.value)}/>
+          <label for="two">Phone</label>
+          <input type="tel" className="phone" name="two" size="10" value={two} onChange={e=>setTwo(e.target.value)}/>
+          <label for="three">E-mail</label>
+          <input type="email" className="e-mail" name="three" size="10"value={three} onChange={e=>setThree(e.target.value)}/>
         </li>
       );
     case "Task":
       return(
         <li>
-          <input type="text" className="task" name="task" />
+          <input type="text" className="task" name="task" value={one} onChange={e=>setOne(e.target.value)}/>
         </li>
       ) ;  
     default:
